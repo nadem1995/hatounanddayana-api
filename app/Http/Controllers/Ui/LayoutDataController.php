472181ef65993e9bbfcbe77835b7884c2746a823
nadem1995 <nadem.ghannam1995@gmail.com>
2026-06-24 10:24:11 +0300

    env config

--- app/Http/Controllers/Ui/LayoutDataController.php
+++ app/Http/Controllers/Ui/LayoutDataController.php
@@ -13,15 +13,9 @@ class LayoutDataController extends Controller
 
     public function index()
     {
-        /* return Cache::remember('layout_data', now()->addHours(6), function () {
-
-            
-        }); */
-
         return response()->json([
             'data' => [
                 'banner' => Banner::select('id', 'statement')->get(),
-
                 'social_links' => SocialMediaSetting::where('status', true)
                     ->select('name', 'url')
                     ->get(),
