30cb7b7395ee71c804be7ff45b1d2861059cc17a
nadem1995 <nadem.ghannam1995@gmail.com>
2026-06-25 22:00:01 +0300

    data layout

--- app/Http/Controllers/Ui/LayoutDataController.php
+++ app/Http/Controllers/Ui/LayoutDataController.php
@@ -4,23 +4,21 @@ namespace App\Http\Controllers\Ui;
 
 use App\Http\Controllers\Controller;
 use App\Models\Banner;
-use Illuminate\Support\Facades\Cache;
 use App\Http\Resources\Ui\BannerResource;
+use Illuminate\Support\Facades\Cache;
 
 
 class LayoutDataController extends Controller
 {
+
     public function index()
     {
-        $banners = Cache::remember('banners', 3600, function () {
-            return Banner::all();
-        });
-
         return response()->json([
             'data' => [
-                'banner' => BannerResource::collection($banners),
+                'banner' => BannerResource::collection(
+                    Banner::all()
+                ),
             ],
         ]);
     }
 }
-
