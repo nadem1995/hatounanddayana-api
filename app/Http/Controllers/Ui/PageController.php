f7dc4be945f63700df32d7d5056389797b2248e6
nadem1995 <nadem.ghannam1995@gmail.com>
2026-06-30 12:49:49 +0300

    pages show fix

--- app/Http/Controllers/Ui/PageController.php
+++ app/Http/Controllers/Ui/PageController.php
@@ -1,19 +1,29 @@
 <?php
-public function show(string $slug)
-{
-    $page = Page::where('slug', $slug)
-        ->firstOrFail();
 
-    return response()->json([
-        'data' => [
-            'id' => $page->id,
-            'title' => app()->getLocale() === 'ar'
-                ? $page->title_ar
-                : $page->title_en,
-            'slug' => $page->slug,
-            'content' => app()->getLocale() === 'ar'
-                ? $page->content_ar
-                : $page->content_en,
-        ],
-    ]);
+namespace App\Http\Controllers\Ui;
+
+use App\Http\Controllers\Controller;
+use App\Models\Page;
+
+class PageController extends Controller
+{
+    public function show(string $slug)
+    {
+        $page = Page::where('status', 1)
+            ->where('slug', $slug)
+            ->firstOrFail();
+
+        return response()->json([
+            'data' => [
+                'id' => $page->id,
+                'title' => app()->getLocale() === 'ar'
+                    ? $page->title_ar
+                    : $page->title_en,
+                'slug' => $page->slug,
+                'content' => app()->getLocale() === 'ar'
+                    ? $page->content_ar
+                    : $page->content_en,
+            ],
+        ]);
+    }
 }
